2223fc2b5c9fefd8df64168edc1a665185d3196a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2016-10-16 18:16:19 +0900

    プロジェクト追加

---
+++ Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+
+namespace UniRxTest
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class AutoDestroy : MonoBehaviour
+	{
+		[SerializeField]
+		private float delay;
+
+		void Start()
+		{
+			Destroy(this.gameObject, this.delay);
+		}
+	}
+}
---
+++ Assets/Scripts/CreatePrefab.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UniRx.Operators;
+using System;
+using UnityEngine.UI;
+
+namespace UniRxTest
+{
+	/// <summary>
+	/// .
+	/// </summary>
+	public class CreatePrefab : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform parent;
+
+		[SerializeField]
+		private GameObject prefab;
+
+		private Queue<int> requestQueue = new Queue<int>();
+
+		private GameObject currentObject = null;
+
+		void Start()
+		{
+			// リクエストがあった場合にプレハブ生成.
+			this.LateUpdateAsObservable()
+				.Where(_ => this.requestQueue.Count > 0 && this.currentObject == null)
+				.Subscribe(x =>
+			{
+				var damage = this.requestQueue.Dequeue();
+				this.currentObject = Instantiate(this.prefab, this.parent, false) as GameObject;
+				this.currentObject.GetComponent<Text>().text = damage.ToString();
+			});
+
+			// リクエストする.
+			this.UpdateAsObservable()
+				.Where(_ => Input.GetMouseButtonDown(0))
+				.Subscribe(_ =>
+			{
+				var damage = UnityEngine.Random.Range(0, 100);
+				this.requestQueue.Enqueue(damage);
+				Debug.Log("Enqueue = " + damage);
+			});
+		}
+
+	}
+}
